1fe060ed689507562753c25fae78fd50953badf8
Cahya Revanto <64825576+revan2159@users.noreply.github.com>
2024-02-17 21:17:17 +0700

    update json

--- src/config/satusehat.php
+++ src/config/satusehat.php
@@ -1,7 +1,12 @@
 <?php
 return
     [
-        'client_id' => getenv('CLIENT_ID_SATUSEHAT'),
-        'client_secret' => getenv('CLIENT_SECRET_SATUSEHAT'),
-        'organization_id' => getenv('ORGANIZATION_ID_SATUSEHAT')
+        'client_id' => getenv('SATUSEHAT_CLIENTID'),
+        'client_secret' => getenv('SATUSEHAT_CLIENTSECRET'),
+        'organization_id' => getenv('SATUSEHAT_ORGID'),
+        'auth_url' => getenv('SATUSEHAT_AUTH'),
+        'base_url' => getenv('SATUSEHAT_BASE'),
+        'consent_url' => getenv('SATUSEHAT_CONSENT'),
+        'kfa_url' => getenv('SATUSEHAT_KFA'),
+        'kyc_url' => getenv('SATUSEHAT_KYC'),
     ];
